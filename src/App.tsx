0c9efa0803fc7af473e5dc0cfe2098b6c0621740
Yididiya Mulatu <yididiyamulatu@gmail.com>
2021-04-09 20:15:03 +0200

    fixing tyoes of useranser

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import QuestionCard from "./components/QuestionCard";
 //Types
 import { QuestionState, Difficulty } from "./API";
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
@@ -60,7 +60,15 @@ function App() {
     }
   };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    //Move on to next q
+    const nextQuestion = number + 1;
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
 
   return (
     <div className="App">
